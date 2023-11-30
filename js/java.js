23453188a082b5da940f86f341ef82cf7f4a4cc2
sohel <145013908+soheldev91@users.noreply.github.com>
2023-11-30 12:33:56 +0600

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,62 @@
+const current = document.getElementById('current')
+console.log(current)
+function one(){
+    current.innerText += '1'
+}
+function two(){
+    current.innerText += '2'
+}
+function three(){
+    current.innerText += '3'
+}
+function five(){
+    current.innerText += '4'
+}
+function six(){
+    current.innerText += '5'
+}
+function seven(){
+    current.innerText += '6'
+}
+function nin(){
+    current.innerText += '7'
+}
+function ten(){
+    current.innerText += '8'
+}
+function eleven(){
+    current.innerText += '9'
+}
+function two(){
+    current.innerText += '0'
+}
+function two(){
+    current.innerText += '='
+}
+function eight(){
+    current.innerText += '+'
+}
+function dor(){
+    current.innerText += '/'
+}
+function tole(){
+    current.innerText += '-'
+}
+function four(){
+    current.innerText += '*'
+}
+function df(){
+    current.innerText += '.'
+}
+function mar(){
+    current.innerText += '%'
+}
+function gar(){
+    current.innerText += '='
+}
+function jan(){
+    current.innerText += 'DEL'
+}
+function iv(){
+    current.innerText += 'AC'
+}
